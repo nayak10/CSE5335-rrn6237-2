d06cb418dba7bb3d2dd4d25a450f5359d250dbce
nayakrajesh <rajesh.nayak@mavs.uta.edu>
2015-11-17 01:05:30 -0600

    changes to query.py

--- query.py
+++ query.py
@@ -7,30 +7,31 @@ import psycopg2
 #url = urlparse.urlparse(os.environ["postgres://flxvzdgthifjqo:YCViVah6QaxrzLgfQoousIxrOC@ec2-54-204-7-145.compute-1.amazonaws.com:5432/d15ik4407jjv1q"])
 
 try:
-    conn = psycopg2.connect("dbname='d43brbvbt6ugl9' user='ywhsrpfxdaknkl' host='ec2-107-21-223-147.compute-1.amazonaws.com' password='dnYTje6Tvi_vDyGq9Z1Qsf5yhY'")
+    conn = psycopg2.connect("dbname='dk3505cd5g85t' user='tajitmcdjbfhsb' host='ec2-107-21-223-72.compute-1.amazonaws.com' password='QIT6b8hSuViOe-iLGdoUdZ-MY-'")
 except psycopg2.Error as e:
     print "I am unable to connect to the database"
 
-idinput = raw_input("Enter IMDB ID to query on: ")    
+idinput = raw_input("Enter Player ID to query on: ")    
 cur = conn.cursor()
-query = "SELECT * from friends where imbdid like " + "'%" + idinput + "%'"
+query = "SELECT * from footballers where playerid = "  + idinput + ";"
 #cur.execute("""SELECT * from friends where imbdid = '%s' """ % (idinput))
 cur.execute(query)
 rows = cur.fetchall()
 
-print "\nShow me the Episode:\n"
+print "\nPlayer Details:\n"
 for row in rows:
-    print "   ", row[0],"-----",row[1],"-----", row[2]
+    print "   ", row[0],"-----",row[1],"-----", row[2],"-----", row[3]
 
 
 low = raw_input("Enter lower bound for IMDB rating")    
 high = raw_input("Enter upper bound for IMDB rating")    
-cur1 = conn.cursor()
-query1 = "SELECT * from friends where imbdrating between "  + low + " and " + high + ";"
 
+cur1 = conn.cursor()
+query1 = "SELECT * from footballers where fifarating between "  + low + " and " + high + ";"
+#query1 = "SELECT * from footballers;"
 cur1.execute(query1)
 rows1 = cur1.fetchall()
 
-print "\nShows between the bound of IMDB ratings:\n"
+print "\nShows players between the bound of FIFA ratings:\n"
 for row1 in rows1:
-    print "   ", row1[0],"-----",row1[1],"-----", row1[2]
+    print "   ", row1[0],"-----",row1[1],"-----", row1[2],"-----", row1[3]
