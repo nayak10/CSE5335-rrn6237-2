6841e369877b98ca929e8d68294a31e80fda8fa7
nayakrajesh <rajesh.nayak@mavs.uta.edu>
2015-11-16 19:52:56 -0600

    changes to query.py

--- query.py
+++ query.py
@@ -1,16 +1,35 @@
 #import os
 import psycopg2
 #import urlparse
+#import traceback
+
+#urlparse.uses_netloc.append("postgres")
+#url = urlparse.urlparse(os.environ["postgres://flxvzdgthifjqo:YCViVah6QaxrzLgfQoousIxrOC@ec2-54-204-7-145.compute-1.amazonaws.com:5432/d15ik4407jjv1q"])
 
 try:
-    conn = psycopg2.connect("dbname='d15ik4407jjv1q' user='flxvzdgthifjqo' host='ec2-54-204-7-145.compute-1.amazonaws.com' password='YCViVah6QaxrzLgfQoousIxrOC'")
-except:
+    conn = psycopg2.connect("dbname='d43brbvbt6ugl9' user='ywhsrpfxdaknkl' host='ec2-107-21-223-147.compute-1.amazonaws.com' password='dnYTje6Tvi_vDyGq9Z1Qsf5yhY'")
+except psycopg2.Error as e:
     print "I am unable to connect to the database"
+    #print e
+    #print e.pgcode
+    #print e.pgerror
+    #print traceback.format_exc()
+'''
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+'''
 
 cur = conn.cursor()
-cur.execute("""SELECT datname from pg_database""")
+cur.execute("""SELECT * from friends""")
 rows = cur.fetchall()
 
-print "\nShow me the databases:\n"
+print "\nShow me the Episodes:\n"
 for row in rows:
-    print "   ", row[0]
+    print "   ", row[0],"-----",row[1],"-----", row[2]
+    #print "   ", row[1]
+    #print "   ", row[2]
