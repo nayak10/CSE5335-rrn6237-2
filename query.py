9ac72d1df4cbb84eb20459ff44fb00d274c0c535
nayakrajesh <rajesh.nayak@mavs.uta.edu>
2015-11-16 19:04:29 -0600

    changes to query.py

--- query.py
+++ query.py
@@ -1,18 +1,11 @@
-import os
+#import os
 import psycopg2
-import urlparse
+#import urlparse
 
-urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["postgres://flxvzdgthifjqo:YCViVah6QaxrzLgfQoousIxrOC@ec2-54-204-7-145.compute-1.amazonaws.com:5432/d15ik4407jjv1q"])
-
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
-
+try:
+    conn = psycopg2.connect("dbname='d15ik4407jjv1q' user='flxvzdgthifjqo' host='ec2-54-204-7-145.compute-1.amazonaws.com' password='YCViVah6QaxrzLgfQoousIxrOC'")
+except:
+    print "I am unable to connect to the database"
 
 cur = conn.cursor()
 cur.execute("""SELECT datname from pg_database""")
